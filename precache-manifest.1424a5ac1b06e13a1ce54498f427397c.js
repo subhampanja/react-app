e5060d5a1954a02d7807d65ffe0842d62f3eea2c
subhampanja <subhampanja28@gmail.com>
2020-06-22 16:00:06 +0530

    Updates

---
+++ precache-manifest.1424a5ac1b06e13a1ce54498f427397c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cd967e68aa008569ef62ca2de18d05",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "bac6037f31e76ad87de1",
+    "url": "/react-app/static/css/main.9b3d0004.chunk.css"
+  },
+  {
+    "revision": "59e97c4881cbc99d3db9",
+    "url": "/react-app/static/js/2.8c25d0ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.8c25d0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac6037f31e76ad87de1",
+    "url": "/react-app/static/js/main.5d9ae430.chunk.js"
+  },
+  {
+    "revision": "a2388a86aabc240346f2",
+    "url": "/react-app/static/js/runtime-main.1127187e.js"
+  }
+]);
